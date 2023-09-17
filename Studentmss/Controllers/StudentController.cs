9b49a0e7ff7ae8cd752e3dfe420e062525085060
Nibas Nazeem <nibas.nazeem@nestgroup.net>
2023-09-17 18:11:33 +0530

    Final Changes

---
+++ Studentmss/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Studentms.Models;
+using Studentms.Repositories;
+
+namespace Studentms.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository _StudentRepository;
+
+        public StudentController(IStudentRepository StudentRepository)
+        {
+            _StudentRepository = StudentRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student Student)
+        {
+            var id = await _StudentRepository.Create(Student);
+
+            return new JsonResult(id.ToString());
+        }
+
+        [HttpGet("get/{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var Student = await _StudentRepository.Get(ObjectId.Parse(id));
+
+            return new JsonResult(Student);
+        }
+
+        [HttpGet("Fetch")]
+        public async Task<IActionResult> Get()
+        {
+            var Students = await _StudentRepository.Get();
+
+            return new JsonResult(Students);
+        }
+
+        [HttpGet("ByName/{Name}")]
+        public async Task<IActionResult> GetByName(string Name)
+        {
+            var Students = await _StudentRepository.GetByName(Name);
+
+            return new JsonResult(Students);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, Student Student)
+        {
+            var result = await _StudentRepository.Update(ObjectId.Parse(id), Student);
+
+            return new JsonResult(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _StudentRepository.Delete(ObjectId.Parse(id));
+
+            return new JsonResult(result);
+        }
+    }
+}
--- Studentmss/Program.cs
+++ Studentmss/Program.cs
@@ -1,3 +1,6 @@
+using MongoDB.Driver;
+using Studentms.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add services to the container.
+builder.Services.AddRazorPages();
+IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory) // Set the base path for appsettings.json
+           .AddJsonFile("appsettings.json") // Add the appsettings.json file
+           .Build();
+
+var mongoClient = new MongoClient(configuration.GetConnectionString("MongoDb"));//set connection string
+builder.Services.AddSingleton<IMongoClient>(mongoClient);
+builder.Services.AddTransient<IStudentRepository, StudentRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
